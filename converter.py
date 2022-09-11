bcfcec3815a4a8e58b39149d327d095dd4499caf
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-09-11 17:42:11 +0430

    add first currency entry

--- converter.py
+++ converter.py
@@ -3,4 +3,8 @@ from tkinter import *
 window = Tk()
 window.config(padx=50, pady=50)
 window.title("Py Currency Converter")
+
+first_currency_entry = Entry(width=20)
+first_currency_entry.grid(row=0, column=0)
+
 window.mainloop()
