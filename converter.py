fd441bf896747f0e601dd8bca7f066eee5c1de89
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-09-11 17:52:46 +0430

    convert second entry to a label

--- converter.py
+++ converter.py
@@ -7,7 +7,8 @@ window.title("Py Currency Converter")
 first_currency_entry = Entry(width=20)
 first_currency_entry.grid(row=0, column=0)
 
-second_currency_entry = Entry(width=20)
-second_currency_entry.grid(row=1, column=0)
+second_currency_label = Label(text="0.00")
+second_currency_label.grid(row=1, column=0)
+second_currency_label.config(pady=20)
 
 window.mainloop()
