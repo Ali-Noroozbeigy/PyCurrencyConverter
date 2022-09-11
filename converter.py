ad48a2af0269096bf3358b62e26c92348ecd2aec
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-09-11 21:42:01 +0430

    add first currency drop down menu

--- converter.py
+++ converter.py
@@ -9,6 +9,13 @@ first_currency_entry.grid(row=0, column=0)
 
 second_currency_label = Label(text="0.00")
 second_currency_label.grid(row=1, column=0)
-second_currency_label.config(pady=20)
+
+currencies = ["USD", "IRR"]
+
+first_currency_drop_menu_value = StringVar()
+first_currency_drop_menu_value.set(currencies[0])
+first_currency_drop_menu = OptionMenu(window, first_currency_drop_menu_value, *currencies)
+first_currency_drop_menu.config(width=7)
+first_currency_drop_menu.grid(row=0, column=1)
 
 window.mainloop()
