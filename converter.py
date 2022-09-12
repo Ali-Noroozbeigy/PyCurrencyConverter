2c76f91449fcd88f250119f45e9d0e3c37fd56e6
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-09-12 19:21:40 +0430

    Add padding to widgets

--- converter.py
+++ converter.py
@@ -5,10 +5,10 @@ window.config(padx=50, pady=50)
 window.title("Py Currency Converter")
 
 first_currency_entry = Entry(width=20)
-first_currency_entry.grid(row=0, column=0)
+first_currency_entry.grid(row=0, column=0, padx=20, pady=20)
 
 second_currency_label = Label(text="0.00")
-second_currency_label.grid(row=1, column=0)
+second_currency_label.grid(row=1, column=0, padx=20, pady=20)
 
 currencies = ["USD", "IRR"]
 
@@ -16,16 +16,16 @@ first_currency_drop_menu_value = StringVar()
 first_currency_drop_menu_value.set(currencies[0])
 first_currency_drop_menu = OptionMenu(window, first_currency_drop_menu_value, *currencies)
 first_currency_drop_menu.config(width=7)
-first_currency_drop_menu.grid(row=0, column=1)
+first_currency_drop_menu.grid(row=0, column=1, padx=20, pady=20)
 
 second_currency_drop_menu_value = StringVar()
 second_currency_drop_menu_value.set(currencies[1])
 second_currency_drop_menu = OptionMenu(window, second_currency_drop_menu_value, *currencies)
 second_currency_drop_menu.config(width=7)
-second_currency_drop_menu.grid(row=1, column=1)
+second_currency_drop_menu.grid(row=1, column=1, padx=20, pady=20)
 
 # TODO: add command for button
 submit_button = Button(text="Convert")
-submit_button.grid(row=2, column=0, columnspan=2)
+submit_button.grid(row=2, column=0, columnspan=2, pady=20)
 
 window.mainloop()
