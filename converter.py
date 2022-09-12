befbf42b7bb43441b988c0efe3b309d059c64640
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-09-12 22:56:31 +0430

    Add focus to entry

--- converter.py
+++ converter.py
@@ -52,6 +52,7 @@ window.config(padx=50, pady=50)
 window.title("Py Currency Converter")
 
 first_currency_entry = Entry(width=20)
+first_currency_entry.focus()
 first_currency_entry.grid(row=0, column=0, padx=20, pady=20)
 
 second_currency_label = Label(text="0.00")
